30d2bf59f38ad1bcd33075865cefd3bf4b99f653
Nick Ficano <nficano@gmail.com>
2016-03-29 17:25:11 -0400

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='python-lambda',
-    version='0.1.0',
+    version='0.1.1',
     description="The bare minimum for a Python app running on Amazon Lambda.",
     long_description=readme + '\n\n' + history,
     author="Nick Ficano",
