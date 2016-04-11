c7f8f457998b038e92e79ec8057f323a31acde23
Nick Ficano <nficano@gmail.com>
2016-04-11 17:20:01 -0400

    added build command

--- aws_lambda/__init__.py
+++ aws_lambda/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Nick Ficano'
 __email__ = 'nficano@gmail.com'
 __version__ = '0.1.11'
 
-from .aws_lambda import deploy, invoke, init
+from .aws_lambda import deploy, invoke, init, build
 
 # Set default logging handler to avoid "No handler found" warnings.
 import logging
