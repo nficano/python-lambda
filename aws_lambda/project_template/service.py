42d11e362f024e89f8347f78f5018f76d50bb23e
Marcel Radischat <sabodendar@googlemail.com>
2016-05-17 19:55:49 +0200

    Add context.json to project_template

--- aws_lambda/project_template/service.py
+++ aws_lambda/project_template/service.py
@@ -3,6 +3,8 @@
 
 def handler(event, context):
     # Your code goes here!
+    context_val = context.client_context.custom.foo
+    assert context_val == 'bar'
     e = event.get('e')
     pi = event.get('pi')
     return e + pi
