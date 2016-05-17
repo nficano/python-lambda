06884ab0ce8e86c2bfea22477af5dd2c8d918f60
Marcel Radischat <marcel@allyapp.com>
2016-05-17 18:49:09 +0200

    Add the feature to mock the context object
    A `MockContext` object is added which mimics the default behaviour
    and implements the necessary attributes and functions described here:
    http://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
    
    The script accepts a `context.json` file. The contents of the file
    can be accessed via the python object `context.client_context.custom`
    in your handler function.

---
+++ aws_lambda/context.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+class MockContext(object):
+    def __init__(self, context):
+        self.function_name = 'Mock'
+        self.function_version = 'm1'
+        self.memory_limit_in_mb = '500'
+        self.aws_request_id = None
+        self.log_groupd_name = None
+        self.log_stream_name = None
+        self.identiy = FakeObject()
+        self.client_context = ClientContext(context)
+
+    def get_remaining_time_in_millis(self):
+        # Returns the remaining execution time, in milliseconds, until
+        # AWS Lambda terminates the function.
+        return 0
+
+
+class FakeObject(object):
+    def __init__(self, context=None):
+        self._context = context or {} # if None was passed
+
+    def __getattr__(self, attr):
+        return self._context.get(attr, None)
+
+
+class ClientContext(object):
+    def __init__(self, context):
+        self.custom = FakeObject(context)
+        self.client = FakeObject()
