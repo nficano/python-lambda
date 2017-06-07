4390aeb81e18a5772df3d8b54641ce3c973f2287
Josh Meekhof <jmeekhof@twotheleft.com>
2017-06-07 13:43:48 +0000

    Issue: #11, #26 Environment variable support
    create_function now has added support to create a lambda environment
    variables that are configured in the config.yaml file.
    
    Newer versions of botocore and boto3 are necessary to keep the
    create_function from complaining about the "Environment" dictionary key.

--- aws_lambda/aws_lambda.py
+++ aws_lambda/aws_lambda.py
@@ -348,29 +348,35 @@ def create_function(cfg, path_to_zip_file):
     client = get_client('lambda', aws_access_key_id, aws_secret_access_key,
                         cfg.get('region'))
 
+    #Do we prefer development variable over config?
     func_name = (
         os.environ.get('LAMBDA_FUNCTION_NAME') or cfg.get('function_name')
     )
     print('Creating lambda function with name: {}'.format(func_name))
-    client.create_function(
-        FunctionName=func_name,
-        Runtime=cfg.get('runtime', 'python2.7'),
-        Role=role,
-        Handler=cfg.get('handler'),
-        Code={'ZipFile': byte_stream},
-        Description=cfg.get('description'),
-        Timeout=cfg.get('timeout', 15),
-        MemorySize=cfg.get('memory_size', 512),
-        Environment={
-            'Variables': {
-                key.strip('LAMBDA_'): value
-                for key, value in os.environ.items()
-                if key.startswith('LAMBDA_')
-            }
-        },
-        Publish=True
-    )
+    kwargs={
+        'FunctionName': func_name,
+        'Runtime': cfg.get('runtime', 'python2.7'),
+        'Role': role,
+        'Handler': cfg.get('handler'),
+        'Code': {'ZipFile': byte_stream},
+        'Description': cfg.get('description'),
+        'Timeout': cfg.get('timeout', 15),
+        'MemorySize': cfg.get('memory_size', 512),
+        'Publish': True
+    }
 
+    if cfg.get('environment_variables', None) is not None:
+        kwargs.update(
+            Environment = {
+                'Variables': {
+                    key: value
+                    for key, value
+                    in cfg.get('environment_variables').items()
+                }
+            }
+        )
+
+    client.create_function( **kwargs )
 
 def update_function(cfg, path_to_zip_file):
     """Updates the code of an existing Lambda function"""
