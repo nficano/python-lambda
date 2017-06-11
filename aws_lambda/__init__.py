060f0d5f3969d36ec3445205d6958df32b92564c
Nick Ficano <nficano@gmail.com>
2017-06-11 00:13:13 -0400

    Bump version: 0.8.0 → 0.8.1

--- aws_lambda/__init__.py
+++ aws_lambda/__init__.py
@@ -2,7 +2,7 @@
 # flake8: noqa
 __author__ = 'Nick Ficano'
 __email__ = 'nficano@gmail.com'
-__version__ = '0.8.0'
+__version__ = '0.8.1'
 
 from .aws_lambda import deploy, invoke, init, build, cleanup_old_versions
 
--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ test_requirements = [
 
 setup(
     name='python-lambda',
-    version='0.8.0',
+    version='0.8.1',
     description="The bare minimum for a Python app running on Amazon Lambda.",
     long_description=readme,
     author="Nick Ficano",
