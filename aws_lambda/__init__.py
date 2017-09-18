3bf165d00840c7c4bc38eeed3b41a2af0ad59a8e
Nick Ficano <nficano@gmail.com>
2017-09-18 12:04:07 -0400

    Bump version: 2.0.1 → 2.1.0

--- aws_lambda/__init__.py
+++ aws_lambda/__init__.py
@@ -2,7 +2,7 @@
 # flake8: noqa
 __author__ = 'Nick Ficano'
 __email__ = 'nficano@gmail.com'
-__version__ = '2.0.1'
+__version__ = '2.1.0'
 
 from .aws_lambda import deploy, invoke, init, build, upload, cleanup_old_versions
 
