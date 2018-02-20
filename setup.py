365f1e913800725c66f97e95f07b6ebe79d5a748
selliott <selliott@edmunds.com>
2018-02-20 08:33:41 -0800

    Bump version: 3.1.2 → 3.1.3

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='python-lambda',
-    version='3.1.2',
+    version='3.1.3',
     description='The bare minimum for a Python app running on Amazon Lambda.',
     long_description=readme,
     author='Nick Ficano',
