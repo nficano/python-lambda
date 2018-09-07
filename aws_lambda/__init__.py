4319ae39e105480a2c6c34fe30bb722c23542621
Gordon Myers <soapergem@gmail.com>
2018-09-07 11:22:49 -0500

    Bump version: 3.2.3 → 3.2.4

--- aws_lambda/__init__.py
+++ aws_lambda/__init__.py
@@ -2,7 +2,7 @@
 # flake8: noqa
 __author__ = 'Nick Ficano'
 __email__ = 'nficano@gmail.com'
-__version__ = '3.2.3'
+__version__ = '3.2.4'
 
 from .aws_lambda import deploy, deploy_s3, invoke, init, build, upload, cleanup_old_versions
 
