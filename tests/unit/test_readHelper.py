81044ad2492084af64b71be5a6c1172ca289974b
Mike Benowitz <mwbenowitz@gmail.com>
2019-04-30 11:26:54 -0400

    Remove test incompatible with 2.7

--- tests/unit/test_readHelper.py
+++ tests/unit/test_readHelper.py
@@ -1,8 +1,8 @@
 import os
 import unittest
 import yaml
-from yaml import YAMLLoadWarning
 from aws_lambda.helpers import read
+
 
 class TestReadHelper(unittest.TestCase):
 
@@ -11,22 +11,22 @@ class TestReadHelper(unittest.TestCase):
     def setUp(self):
         with open(TestReadHelper.TEST_FILE, 'w') as tmp_file:
             tmp_file.write('testYaml: testing')
-    
+
     def tearDown(self):
         os.remove(TestReadHelper.TEST_FILE)
-    
+
     def test_read_no_loader_non_binary(self):
         fileContents = read(TestReadHelper.TEST_FILE)
         self.assertEqual(fileContents, 'testYaml: testing')
-    
+
     def test_read_yaml_loader_non_binary(self):
         testYaml = read(TestReadHelper.TEST_FILE, loader=yaml.full_load)
         self.assertEqual(testYaml['testYaml'], 'testing')
-    
+
     def test_read_no_loader_binary_mode(self):
         fileContents = read(TestReadHelper.TEST_FILE, binary_file=True)
         self.assertEqual(fileContents, b'testYaml: testing')
-    
+
     def test_read_yaml_loader_binary_mode(self):
         testYaml = read(
             TestReadHelper.TEST_FILE,
@@ -34,8 +34,3 @@ class TestReadHelper(unittest.TestCase):
             binary_file=True
         )
         self.assertEqual(testYaml['testYaml'], 'testing')
-    
-    def test_read_yaml_old_load_warns(self):
-        with self.assertWarns(YAMLLoadWarning):
-            testYaml = read(TestReadHelper.TEST_FILE, loader=yaml.load)
-            self.assertEqual(testYaml['testYaml'], 'testing')
