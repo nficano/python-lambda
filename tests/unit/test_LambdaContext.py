c6e1774c8c8d9b41509e183df2177f32427274df
Mike Benowitz <mwbenowitz@gmail.com>
2019-04-30 10:46:03 -0400

    Fix second/millisecond conversion issue in test case
    The test for the `get_remaining_time_in_millis` method was
    evaluating the output against a count in seconds, not milliseconds.
    This corrects the issue to allow the test suite to pass

--- tests/unit/test_LambdaContext.py
+++ tests/unit/test_LambdaContext.py
@@ -5,9 +5,9 @@ import unittest
 class TestLambdaContext(unittest.TestCase):
 
 	def test_get_remaining_time_in_millis(self):
-		context = LambdaContext('function_name',2000)
+		context = LambdaContext('function_name', 2000)
 		time.sleep(.5)
-		self.assertTrue(context.get_remaining_time_in_millis() < 2000)
+		self.assertTrue(context.get_remaining_time_in_millis() < 2000000)
 
 
 if __name__ == '__main__':
