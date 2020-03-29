26099ba953eb383347efbf371bfeee93f344da14
Nick Ficano <nficano@gmail.com>
2020-03-29 11:58:52 -0400

    Bump version: 10.0.4 → 11.0.0

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ class UploadCommand(Command):
 
 setup(
     name="python-lambda",
-    version="10.0.4",
+    version="11.0.0",
     author="Nick Ficano",
     author_email="nficano@gmail.com",
     packages=find_packages(),
